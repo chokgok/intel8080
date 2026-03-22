534c26a383fb256011d939b5950538324a655a42
kilroy <>
2026-03-22 01:19:00 -0400

    begin emulator development

---
+++ emulator/i8080/i8080.cpp
@@ -0,0 +1,5 @@
+#include <i8080.h>
+
+void i8080::hello() {
+    printf("hello from i8080!\n");
+}
---
+++ emulator/i8080/i8080.h
@@ -0,0 +1,10 @@
+#ifndef I8080_H
+#define I8080_H
+
+#include <cstdio>
+
+namespace i8080 {
+    void hello();
+}
+
+#endif // I8080_H
